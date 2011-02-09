9144231566ab7237e8fcae499d60eab864b79739
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-09 23:41:39 +0100

    connection

--- src/mod_syslog.erl
+++ src/mod_syslog.erl
@@ -11,7 +11,9 @@
 	start/2,
 	stop/1,
 	user_available/1,
-	presence_update/4
+	presence_update/4,
+	register_connection/3,
+	remove_connection/3
 ]).
 
 start(Host, _Opts) ->
@@ -21,6 +23,10 @@ start(Host, _Opts) ->
 		?MODULE, user_available, 50),
 	ejabberd_hooks:add(unset_presence_hook, Host,
 		?MODULE, presence_update, 50),
+	ejabberd_hooks:add(sm_register_connection_hook, Host,
+			?MODULE, register_connection, 50),
+	ejabberd_hooks:add(sm_remove_connection_hook, Host,
+		?MODULE, remove_connection, 50),
 	ok.
 
 stop(_Host) ->
@@ -33,4 +39,17 @@ user_available(#jid{luser = LUser, lserver = LServer} = _JID) ->
 
 presence_update(User, Server, Resource, Status) ->
 	syslog:send(ejabberd, info, User ++ "@" ++ Server ++ "/" ++ Resource ++ " is " ++ Status),
+	ok.
+
+register_connection(SID, JID, Info) ->
+	?INFO_MSG("SID ~p", [SID]),
+	?INFO_MSG("JID ~p", [JID]),
+	?INFO_MSG("Info ~p", [Info]),
+	%%dict:fetch(ip, Info)
+	syslog:send(ejabberd, info, jlib:jid_to_string(JID) ++ io_lib:format(" open connection from ~p", [element(1,element(2,lists:keyfind(ip, 1, Info)))])),
+	ok.
+
+remove_connection(SID, JID, Info) ->
+	syslog:send(ejabberd, info, jlib:jid_to_string(JID) ++ " close connection" ++ io_lib:format(" ~p", [SID])),
+	%% ++ " close connection " ++ SID ++ " : " ++ Info),
 	ok.
