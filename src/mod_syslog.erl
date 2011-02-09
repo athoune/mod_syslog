c9037664301fd9c0733e932e2115cbcb20e4d490
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-09 21:57:48 +0100

    status

--- src/mod_syslog.erl
+++ src/mod_syslog.erl
@@ -10,14 +10,17 @@
 -export([
 	start/2,
 	stop/1,
-	user_available/1
+	user_available/1,
+	presence_update/4
 ]).
 
 start(Host, _Opts) ->
 	application:start(syslog),
 	syslog:send(ejabberd, info, "starting mod_syslog"),
 	ejabberd_hooks:add(user_available_hook, Host,
-	       ?MODULE, user_available, 50),
+		?MODULE, user_available, 50),
+	ejabberd_hooks:add(unset_presence_hook, Host,
+		?MODULE, presence_update, 50),
 	ok.
 
 stop(_Host) ->
@@ -26,4 +29,8 @@ stop(_Host) ->
 
 user_available(#jid{luser = LUser, lserver = LServer} = _JID) ->
 	syslog:send(ejabberd, info, LUser ++ "@" ++ LServer ++ " is available"),
+	ok.
+
+presence_update(User, Server, Resource, Status) ->
+	syslog:send(ejabberd, info, User ++ "@" ++ Server ++ "/" ++ Resource ++ " is " ++ Status),
 	ok.
