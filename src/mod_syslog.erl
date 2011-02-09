ad78518bea6ebaeb3348c837304004f397aca466
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-09 21:51:15 +0100

    available

--- src/mod_syslog.erl
+++ src/mod_syslog.erl
@@ -9,14 +9,21 @@
 
 -export([
 	start/2,
-	stop/1
+	stop/1,
+	user_available/1
 ]).
 
-start(Host, Opts) ->
+start(Host, _Opts) ->
 	application:start(syslog),
 	syslog:send(ejabberd, info, "starting mod_syslog"),
+	ejabberd_hooks:add(user_available_hook, Host,
+	       ?MODULE, user_available, 50),
 	ok.
-	
-stop(Host) ->
+
+stop(_Host) ->
 	syslog:send(ejabberd, info, "stopping mod_syslog"),
+	ok.
+
+user_available(#jid{luser = LUser, lserver = LServer} = _JID) ->
+	syslog:send(ejabberd, info, LUser ++ "@" ++ LServer ++ " is available"),
 	ok.
