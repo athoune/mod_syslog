e59816afbafb07238de53393f997fd9180608efa
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-12 19:54:25 +0100

    all in macro

--- src/mod_syslog.erl
+++ src/mod_syslog.erl
@@ -21,7 +21,7 @@ start(Host, Opts) ->
 	?INFO_MSG("options ~p", [Opts]),
 	?INFO_MSG("elements ~p", [element(2, lists:keyfind(modules, 1, Opts))]),
 	application:start(syslog),
-	syslog:send(ejabberd, info, "starting mod_syslog"),
+	?SYSLOG_INFO(ejabberd, "starting mod_syslog"),
 	lists:foreach(fun(Elem)->
 		?INFO_MSG("element ~p", [Elem]),
 		case Elem of
@@ -30,13 +30,13 @@ start(Host, Opts) ->
 						?MODULE, register_connection, 50),
 				ejabberd_hooks:add(sm_remove_connection_hook, Host,
 					?MODULE, remove_connection, 50),
-					syslog:send(ejabberd, info, "mod_syslog enabling connection module");
+					?SYSLOG_INFO(ejabberd, "mod_syslog enabling connection module");
 			presence ->
 				ejabberd_hooks:add(user_available_hook, Host,
 					?MODULE, user_available, 50),
 				ejabberd_hooks:add(unset_presence_hook, Host,
 					?MODULE, presence_update, 50),
-				syslog:send(ejabberd, info, "mod_syslog enabling presence module");
+				?SYSLOG_INFO(ejabberd, "mod_syslog enabling presence module");
 			_ -> ok
 		end
 	end, element(2, lists:keyfind(modules, 1, Opts))),
@@ -48,11 +48,11 @@ stop(_Host) ->
 	ok.
 
 user_available(#jid{luser = LUser, lserver = LServer} = _JID) ->
-	syslog:send(ejabberd, info, LUser ++ "@" ++ LServer ++ " is available"),
+	?SYSLOG_INFO(ejabberd, LUser ++ "@" ++ LServer ++ " is available"),
 	ok.
 
 presence_update(User, Server, Resource, Status) ->
-	syslog:send(ejabberd, info, User ++ "@" ++ Server ++ "/" ++ Resource ++ " is " ++ Status),
+	?SYSLOG_INFO(ejabberd, User ++ "@" ++ Server ++ "/" ++ Resource ++ " is " ++ Status),
 	ok.
 
 register_connection(SID, JID, Info) ->
@@ -61,12 +61,12 @@ register_connection(SID, JID, Info) ->
 	?INFO_MSG("Info ~p", [Info]),
 	%%dict:fetch(ip, Info)
 	IP = element(1,element(2,lists:keyfind(ip, 1, Info))),
-	syslog:send(ejabberd, info, jlib:jid_to_string(JID) ++
+	?SYSLOG_INFO(mod_syslog,jlib:jid_to_string(JID) ++
 		io_lib:format(" open connection from ~B.~B.~B.~B", [element(1, IP), element(2, IP), element(3, IP), element(4, IP)])),
 	ok.
 
 remove_connection(_SID, JID, _Info) ->
-	syslog:send(ejabberd, info, jlib:jid_to_string(JID) ++ " close connection"),
+	?SYSLOG_INFO(ejabberd, jlib:jid_to_string(JID) ++ " close connection"),
 	%% ++ io_lib:format(" ~p", [SID])
 	%% ++ " close connection " ++ SID ++ " : " ++ Info),
 	ok.
